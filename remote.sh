2428235e137002ed30c64e25cbe208d1622b7d2b
chen-wilde <151738787+chen-wilde@users.noreply.github.com>
2025-06-27 15:00:29 +0800

    test

---
+++ remote.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+# https://github.com/chen-wilde/Actions-OpenWrt
+#
+# File name: remote.sh
+# Description: OpenWrt script for create remote config (Before diy script part 2)
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+
+mkdir -p files/etc/config
+cat > files/etc/config/tailscale << EOF
+
+config tailscale 'settings'
+    option enabled '1'
+    option fw_mode 'iptables'
+    option port '41641'
+    option config_path '/etc/tailscale'
+    option state_file '/etc/tailscale/tailscaled.state'
+    option log_stdout '1'
+    option log_stderr '1'
+    option acceptRoutes '0'
+    option acceptDNS '1'
+    option advertiseExitNode '0'
+    list access 'tsfwlan'
+    list access 'tsfwwan'
+    list access 'lanfwts'
+    list flags '--auth-key=$auth_key'
+EOF
+
+echo "$FRPC_CONFIG" > files/etc/config/frpc
+echo "$TUNNEL_CONFIG" > files/etc/config/cloudflared
+echo "$ZEROTIER_CONFIG" > files/etc/config/zerotier
+
+sed -i "s/\\\$1\\\$[^:]*:0:/$LEDE_PASSWD/g" openwrt/package/lean/default-settings/files/zzz-default-settings
