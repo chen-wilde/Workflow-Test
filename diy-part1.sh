5d51de91adba2c7ad826491026a25189f2342067
chen-wilde <151738787+chen-wilde@users.noreply.github.com>
2026-03-09 23:06:20 +0800

    Add h68k test

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,6 +12,8 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 #sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+ sed -i '1i src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git' feeds.conf.default
+ sed -i '1i src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git' feeds.conf.default
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
